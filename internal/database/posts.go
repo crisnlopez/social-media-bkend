b93e91eaf7121d9dc6aa6693d1fc83158230dee5
crisnlopez <79811937+crisnlopez@users.noreply.github.com>
2022-02-25 00:10:49 -0300

    Create, Get and Delete post funcitions

--- internal/database/posts.go
+++ internal/database/posts.go
@@ -1,6 +1,11 @@
 package database
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 // Post -
 type Post struct {
@@ -8,4 +13,69 @@ type Post struct {
 	CreatedAt time.Time `json:"createdAt"`
 	UserEmail string    `json:"userEmail"`
 	Text      string    `json:"text"`
+}
+
+func (c Client) CreatePost(userEmail, text string) (Post, error) {
+	db, err := c.readDB()
+	if err != nil {
+		return Post{}, err
+	}
+
+	_, ok := db.Users[userEmail]
+	if !ok {
+		return Post{}, errors.New("the user doesn´t exist")
+	}
+
+	id := uuid.NewString()
+	newPost := Post{
+		ID:        id,
+		CreatedAt: time.Now().UTC().Local(),
+		UserEmail: userEmail,
+		Text:      text,
+	}
+	db.Post[id] = newPost
+
+	err = c.updateDB(db)
+	if err != nil {
+		return Post{}, err
+	}
+
+	return newPost, nil
+}
+
+func (c Client) GetPosts(userEmail string) ([]Post, error) {
+	db, err := c.readDB()
+	if err != nil {
+		return []Post{}, err
+	}
+
+	_, ok := db.Users[userEmail]
+	if !ok {
+		return []Post{}, errors.New("the user doesn´t exist")
+	}
+
+	var posts []Post
+	for _, v := range db.Post {
+		if v.UserEmail == userEmail {
+			posts = append(posts, v)
+		}
+	}
+
+	return posts, err
+}
+
+func (c Client) DeletePost(id string) error {
+	db, err := c.readDB()
+	if err != nil {
+		return err
+	}
+
+	delete(db.Post, id)
+
+	err = c.updateDB(db)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
