8e5e3ec06bcfa4f603657eff1e26b4c1b0575521
crisnlopez <79811937+crisnlopez@users.noreply.github.com>
2022-02-25 15:37:37 -0300

    apiConfig has endpointUsersHandler:  - routes to specific handler function depending on HTTP method in http.Request data.  - apiConfig handlerCreateUser for POST method

--- main.go
+++ main.go
@@ -7,13 +7,26 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/crisnlopez/social-media-bkend/internal/database"
+	db "github.com/crisnlopez/social-media-bkend/internal/database"
 )
 
 func main() {
+	// Create database
+	dbClient := db.NewClient("db.json")
+	err := dbClient.EnsureDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	apiCfg := apiConfig{
+		dbClient: dbClient,
+	}
+
+	// Router
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", testHandler)
 	mux.HandleFunc("/err", testErrHanlder)
+	mux.HandleFunc("/users", apiCfg.endpointUsersHandler)
+	mux.HandleFunc("/users/", apiCfg.endpointUsersHandler)
 
 	srv := http.Server{
 		Addr:         "localhost:8080",
@@ -22,12 +35,56 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	log.Fatal(err)
 }
 
+type apiConfig struct {
+	dbClient db.Client
+}
+
+func (apiCfg apiConfig) endpointUsersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// call  GET handler
+	case http.MethodPost:
+		//call POST handler
+		apiCfg.handlerCreateUser(w, r)
+	case http.MethodPut:
+		//call PUT handler
+	case http.MethodDelete:
+		//call DELETE handler
+	default:
+		responseWithError(w, 404, errors.New("method not supported"))
+	}
+}
+
+// Post handler reads the request body and creates a user with the given parameters
+func (apiCfg apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+		Name     string `json:"name"`
+		Age      int    `json:"age"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params) // Store decode JSON in params struct
+	if err != nil {
+		responseWithError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	user, err := apiCfg.dbClient.CreateUser(params.Email, params.Password, params.Name, params.Age)
+	if err != nil {
+		responseWithError(w, http.StatusInternalServerError, err)
+	}
+	respondWithJSON(w, http.StatusCreated, user)
+}
+
 func testHandler(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, 200, database.User{
+	respondWithJSON(w, 200, db.User{
 		Email:    "test@example.com",
 		Password: "Password",
 		Name:     "Pepe",
@@ -46,7 +103,7 @@ type errorBody struct {
 
 func responseWithError(w http.ResponseWriter, code int, err error) {
 	log.Println(err)
-	respondWithJSON(w, 500, errorBody{
+	respondWithJSON(w, code, errorBody{
 		Error: err.Error(),
 	})
 }
