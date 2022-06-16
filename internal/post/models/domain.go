2795a8bfd3022142853706be09093810a78985af
cris <cristianpo@live.com.ar>
2022-06-16 11:38:54 -0300

    fix format

--- internal/post/models/domain.go
+++ internal/post/models/domain.go
@@ -3,20 +3,20 @@ package models
 import "time"
 
 type Post struct {
-  ID int64 `json:"id"`
-  UserID int64 `json:"user_id"`
-  Text string `json:"text"`
-  CreatedAt time.Time `json:"created_at"`
-  UpdateAt time.Time `json:"update_at"`
+	ID        int64     `json:"id"`
+	UserID    int64     `json:"user_id"`
+	Text      string    `json:"text"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdateAt  time.Time `json:"update_at"`
 }
 
 type PostCreateRequest struct {
-  UserID int64 `json:"user_id"`
-  Text string `json:"text"`
+	UserID int64  `json:"user_id"`
+	Text   string `json:"text"`
 }
 
 type PostUpdateRequest struct {
-  ID int64 `json:"id"`
-  UserID int64 `json:"user_id"`
-  Text string `json:"text"`
+	ID     int64  `json:"id"`
+	UserID int64  `json:"user_id"`
+	Text   string `json:"text"`
 }
